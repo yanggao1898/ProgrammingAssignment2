7ede020d5c27320a29d8e81dcd9dde2646b3ebea
Yang <yanggao1898@gmail.com>
2014-11-23 01:24:18 -0500

    Filled in the codestubs
    Implemented both makeCacheMatrix and cacheSolve

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,41 @@
 ## functions do
 
 ## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+## On creation, stores matrix into mtx, sets up functions to:
+## set a matrix as the new matrix. At the same time remove the cached matrix inverse
+## get the current matrix
+## set the matrix inverse
+## get the matrix inverse
+makeCacheMatrix <- function(mtx = matrix()) {
+	mInv = NULL
+	set = function(newMatrix) {
+		mtx <<- newMatrix
+		mInv <<- NULL
+	}
+	
+	get = function() return mtx
+	setInverse = function(inverse) mInv <<- inverse
+	getInverse = function() return mInv
+	list(set = set, get = get, 
+		 setInverse = setInverse, 
+		 getInverse = getInverse)
 }
 
 
 ## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## tries to get the matrix inverse of the special matrix class
+## if the cache is not empty, return the cached value
+## otherwise, get the original matrix, perform the inversion,
+## set the inversion, and return it.
+cacheSolve <- function(mCM, ...) {
+	## Return a matrix that is the inverse of 'x'
+	mInv = mCM$getInverse()
+	if(!is.null(mInv)) {
+		message("getting cached data")
+		return(mInv)
+	}
+	data = mCM$get()
+	mInv = solve(data)
+	mCM$setInverse(mInv)
+	return mInv
 }
